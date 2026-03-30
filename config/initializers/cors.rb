6cb4c3c160c06825324e6702feee42c3aa820fe2
GyuhaWang <ethanleast@gmail.com>
2026-03-30 17:10:47 +0900

    fix: cors nil env for CI

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV["FRONTEND_URL"], "http://localhost:3000"
+    origins ENV.fetch("FRONTEND_URL", "http://localhost:3000")
 
     resource "*",
       headers: :any,
