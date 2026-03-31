434c3ffa72e64e30a90d6f5feb3f240cbc3987e3
GyuhaWang <ethanleast@gmail.com>
2026-03-31 19:33:08 +0900

    FIX: CI/TEST

--- Gemfile
+++ Gemfile
@@ -37,7 +37,7 @@ gem "rack-cors"
 gem "bcrypt", "~> 3.1.7"
 gem "omniauth"
 gem "omniauth-google-oauth2"
-gem "dotenv-rails", groups: [:development, :test]
+gem "dotenv-rails", groups: %i[development test]
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- db/migrate/20260331081756_add_google_auth_fields_to_users.rb
+++ db/migrate/20260331081756_add_google_auth_fields_to_users.rb
@@ -7,7 +7,7 @@ class AddGoogleAuthFieldsToUsers < ActiveRecord::Migration[8.1]
     add_column :users, :email_verified, :boolean, default: false, null: false
     add_column :users, :last_sign_in_at, :datetime
 
-    add_index :users, [:provider, :provider_uid], unique: true
+    add_index :users, %i[provider provider_uid], unique: true
     add_index :users, :nickname, unique: true
     add_index :users, :email, unique: true
   end
